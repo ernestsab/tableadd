e0b55758ea963536a003840aece53f5ad3387214
Andrei <andreibunescu.m@gmail.com>
2017-09-20 20:36:43 +0200

    web.clj

--- src/tableadd/web.clj
+++ src/tableadd/web.clj
@@ -27,4 +27,4 @@
   (let [port (Integer. (or (System/getenv "PORT") "8080"))] (start port))
   (apply sql/insert! item/spec :items 
                ([:first_name :last_name :phone_number :kenmerk_gebruiker]
-               [json/first-name-values json/last-name-values json/phone-values json/kenmerk-values]))
+               [json/first-name-values json/last-name-values json/phone-values json/kenmerk-values])))
