3228dea372d90274c46db38de8e24215f04ee4a0
Andrei <andreibunescu.m@gmail.com>
2017-09-21 11:14:35 +0200

    json.clj and web.clj

--- src/tableadd/json.clj
+++ src/tableadd/json.clj
@@ -14,12 +14,9 @@
 
 
 (def first-name-obj (.createArrayOf con "varchar" (into-array String first-name-values)))
-;(def last-name-obj
-;  (.createArrayOf item/spec "varchar" (into-array String last-name-values)))
-;(def phone-obj
-;  (.createArrayOf item/spec "varchar" (into-array String phone-values)))
-;(def kenmerk-obj
-;  (.createArrayOf item/spec "varchar" (into-array String kenmerk-values)))
+(def last-name-obj (.createArrayOf con "varchar" (into-array String last-name-values)))
+(def phone-obj (.createArrayOf con "varchar" (into-array String phone-values)))
+(def kenmerk-obj (.createArrayOf con "varchar" (into-array String kenmerk-values)))
 
 (defn create-task [task-name]
   (sql/insert! item/spec
