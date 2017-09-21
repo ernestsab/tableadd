97bbc76c1d696beba9f3c83830668493d3a65cd6
Andrei <andreibunescu.m@gmail.com>
2017-09-21 11:39:57 +0200

    json.clj

--- src/tableadd/json.clj
+++ src/tableadd/json.clj
@@ -5,6 +5,21 @@
 
 (def con (sql/get-connection item/spec))
 
+(extend-protocol clojure.java.jdbc/ISQLParameter
+  clojure.lang.IPersistentVector
+  (set-parameter [v ^java.sql.PreparedStatement stmt ^long i]
+    (let [con (.getConnection stmt)
+          meta (.getParameterMetaData stmt)
+          type-name (.getParameterTypeName meta i)]
+      (if-let [elem-type (when (= (first type-name) \_) (apply str (rest type-name)))]
+        (.setObject stmt i (.createArrayOf con elem-type (to-array v)))
+        (.setObject stmt i v)))))
+
+(extend-protocol clojure.java.jdbc/IResultSetReadColumn
+  java.sql.Array
+  (result-set-read-column [val _ _]
+    (into [] (.getArray val))))
+
 (def maps-vector (json/read-str (slurp "http://pharmit.daanstreng.nl/")))
 (def first-name-values (into [] (map #(get % "FirstName") maps-vector)))
 (def last-name-values (into [] (map #(get % "LastName") maps-vector)))
