732ac98b6f999342fc85da9569dfba8d4a7fa5e1
Andrei <andreibunescu.m@gmail.com>
2017-09-22 13:57:01 +0200

    web.clj

--- src/tableadd/web.clj
+++ src/tableadd/web.clj
@@ -28,16 +28,6 @@
   (let [port (Integer. (or (System/getenv "PORT") "8080"))] (start port))
   (sql/insert! item/spec :items 
          [:first_name :last_name :phone_number :kenmerk_gebruiker]
-         ["Thorax" "TestgebruikerZvJ1" "033-4659115" "3097"] 
-         ["Test" "Wolfpacktest1" "01010101" "3099"] 
-         ["Test" "Wolfpacktest3" "90340909" "3101"] 
-         ["Test" "Wolfpacktest2" "01010101" "3100"] 
-         ["Test" "Wolfpacktest5" "0101010010101" "3103"] 
-         ["Test" "Wolfpacktest4" "0101010010101" "3102"]
-         ["Test" "Wolfpacktest8" "010101001011100" "3106"] 
-         ["Test" "Wolfpacktest6" "01010101010010101" "3104"] 
-         ["Test" "Wolfpacktest7" "1010101010" "3105"] 
-         ["Thorax" "TestgebruikerZvJ2" "033 465 91 15" "3095"] 
-         ["Thorax" "TestgebruikerZvJBeheer" "033-46591152" "3094"]))
+         ["Thorax" "TestgebruikerZvJ1" "033-4659115" "3097"]))
                                
   
