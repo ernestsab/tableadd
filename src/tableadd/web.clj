0273988abcf0b9fd6ea5dd759e8591fdc7a5939b
Andrei <andreibunescu.m@gmail.com>
2017-09-22 13:59:58 +0200

    web.clj

--- src/tableadd/web.clj
+++ src/tableadd/web.clj
@@ -28,6 +28,7 @@
   (let [port (Integer. (or (System/getenv "PORT") "8080"))] (start port))
   (sql/insert! item/spec :items 
          [:first_name :last_name :phone_number :kenmerk_gebruiker]
-         ["Thorax" "TestgebruikerZvJ1" "033-4659115" "3097"]))
+         ["Thorax" "TestgebruikerZvJ1" "033-4659115" "3097"]
+         ["Test" "Wolfpacktest3" "90340909" "3101"]))
                                
   
