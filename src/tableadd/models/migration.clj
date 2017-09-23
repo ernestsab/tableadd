03691c3214e3e6ad3b76546f80bb16ec66a91690
Andrei <andreibunescu.m@gmail.com>
2017-09-23 20:36:36 +0200

    migration.clj

--- src/tableadd/models/migration.clj
+++ src/tableadd/models/migration.clj
@@ -14,10 +14,10 @@
     (sql/db-do-commands item/spec
                         (sql/create-table-ddl 
                         :items
-                         [:id :serial "PRIMARY KEY"]
+                         [[:id :serial "PRIMARY KEY"]
                          [:first_name :varchar "NOT NULL"]
                          [:last_name :varchar "NOT NULL"]
                          [:phone_number :varchar "NOT NULL"]
                          [:kenmerk_gebruiker :varchar "NOT NULL"]
-                         [:created_at :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]))
+                         [:created_at :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]]))
     (println " done")))
