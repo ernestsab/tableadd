010d0a3ab01a4c28036acd29864a93140c95b9fa
Andrei <andreibunescu.m@gmail.com>
2017-09-23 21:31:43 +0200

    json.clj

--- src/tableadd/json.clj
+++ src/tableadd/json.clj
@@ -34,7 +34,7 @@
 ; (def kenmerk-obj (.createArrayOf con "varchar" (into-array String kenmerk-values)))
 
 
-(def args-list '(["Thorax" "TestgebruikerZvJ1" "033-4659115" "3097"] 
+(def args-list (["Thorax" "TestgebruikerZvJ1" "033-4659115" "3097"] 
  ["Test" "Wolfpacktest1" "01010101" "3099"] 
  ["Test" "Wolfpacktest3" "90340909" "3101"] 
  ["Test" "Wolfpacktest2" "01010101" "3100"] 
@@ -54,14 +54,4 @@
 
 (defn json-insert []
   (apply sql/insert! item/spec :items 
-               [:first_name :last_name :phone_number :kenmerk_gebruiker]
-               ["Test" "Wolfpacktest1" "01010101" "3099"] 
-               ["Test" "Wolfpacktest3" "90340909" "3101"] 
-               ["Test" "Wolfpacktest2" "01010101" "3100"] 
-               ["Test" "Wolfpacktest5" "0101010010101" "3103"] 
-               ["Test" "Wolfpacktest4" "0101010010101" "3102"]
-               ["Test" "Wolfpacktest8" "010101001011100" "3106"] 
-               ["Test" "Wolfpacktest6" "01010101010010101" "3104"] 
-               ["Test" "Wolfpacktest7" "1010101010" "3105"] 
-               ["Thorax" "TestgebruikerZvJ2" "033 465 91 15" "3095"] 
-               ["Thorax" "TestgebruikerZvJBeheer" "033-46591152" "3094"]))
+               [:first_name :last_name :phone_number :kenmerk_gebruiker] args-list))
