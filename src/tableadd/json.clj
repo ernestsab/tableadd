f91e4aa3550665add65adbd400bd8e0d2e4f5ac2
Andrei <andreibunescu.m@gmail.com>
2017-09-24 13:47:21 +0200

    json.clj

--- src/tableadd/json.clj
+++ src/tableadd/json.clj
@@ -3,22 +3,22 @@
             [tableadd.models.item :as item]
             [clojure.java.jdbc :as sql]))
 
-; (def con (sql/get-connection item/spec))
+ (def con (sql/get-connection item/spec))
 
-; (extend-protocol clojure.java.jdbc/ISQLParameter
-;   clojure.lang.IPersistentVector
-;   (set-parameter [v ^java.sql.PreparedStatement stmt ^long i]
-;     (let [con (.getConnection stmt)
-;           meta (.getParameterMetaData stmt)
-;           type-name (.getParameterTypeName meta i)]
-;       (if-let [elem-type (when (= (first type-name) \_) (apply str (rest type-name)))]
-;         (.setObject stmt i (.createArrayOf con elem-type (to-array v)))
-;         (.setObject stmt i v)))))
+(extend-protocol clojure.java.jdbc/ISQLParameter
+  clojure.lang.IPersistentVector
+  (set-parameter [v ^java.sql.PreparedStatement stmt ^long i]
+    (let [con (.getConnection stmt)
+          meta (.getParameterMetaData stmt)
+          type-name (.getParameterTypeName meta i)]
+      (if-let [elem-type (when (= (first type-name) \_) (apply str (rest type-name)))]
+        (.setObject stmt i (.createArrayOf con elem-type (to-array v)))
+        (.setObject stmt i v)))))
 
-; (extend-protocol clojure.java.jdbc/IResultSetReadColumn
-;   java.sql.Array
-;   (result-set-read-column [val _ _]
-;     (into [] (.getArray val))))
+(extend-protocol clojure.java.jdbc/IResultSetReadColumn
+  java.sql.Array
+  (result-set-read-column [val _ _]
+    (into [] (.getArray val))))
 
 (def maps-vector (json/read-str (slurp "http://pharmit.daanstreng.nl/") :key-fn keyword))
 (def first-name-values (into [] (map #(get % "FirstName") maps-vector)))
