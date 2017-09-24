beec984bbc408de4bdc704ca57ecc63148a9c4fd
Andrei <andreibunescu.m@gmail.com>
2017-09-24 14:17:51 +0200

    json.clj

--- src/tableadd/json.clj
+++ src/tableadd/json.clj
@@ -34,7 +34,7 @@
 ; (def kenmerk-obj (.createArrayOf con "varchar" (into-array String kenmerk-values)))
 
 
-(def args-list '(["Thorax" "TestgebruikerZvJ1" "033-4659115" "3097"] 
+(def args-list [["Thorax" "TestgebruikerZvJ1" "033-4659115" "3097"] 
  ["Test" "Wolfpacktest1" "01010101" "3099"] 
  ["Test" "Wolfpacktest3" "90340909" "3101"] 
  ["Test" "Wolfpacktest2" "01010101" "3100"] 
@@ -44,16 +44,16 @@
  ["Test" "Wolfpacktest6" "01010101010010101" "3104"] 
  ["Test" "Wolfpacktest7" "1010101010" "3105"] 
  ["Thorax" "TestgebruikerZvJ2" "033 465 91 15" "3095"] 
- ["Thorax" "TestgebruikerZvJBeheer" "033-46591152" "3094"]))
+ ["Thorax" "TestgebruikerZvJBeheer" "033-46591152" "3094"]])
 
 ;(defn create-task [task-name]
  ; (sql/insert! item/spec
   ;             :items [:first_name] [task-name])
   ;(println (str "Adding task " task-name " right now")))
   
-(def rows (sql/.createArrayOf (repeat 10 ["Thorax" "TestgebruikerZvJ1" "033-4659115" "3097"])))
+(def rows (repeat 10 ["Thorax" "TestgebruikerZvJ1" "033-4659115" "3097"]))
 
 
 (defn json-insert []
   (apply sql/insert! item/spec :items 
-      [:first_name :last_name :phone_number :kenmerk_gebruiker] rows))
+      [:first_name :last_name :phone_number :kenmerk_gebruiker] args-list))
