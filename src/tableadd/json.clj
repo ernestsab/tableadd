213749469b3a0cf2da252595a499bb0b06db0cfd
Andrei <andreibunescu.m@gmail.com>
2017-09-24 14:22:11 +0200

    json.clj

--- src/tableadd/json.clj
+++ src/tableadd/json.clj
@@ -34,8 +34,7 @@
 ; (def kenmerk-obj (.createArrayOf con "varchar" (into-array String kenmerk-values)))
 
 
-(def args-list {["Thorax" "TestgebruikerZvJ1" "033-4659115" "3097"] 
- ["Test" "Wolfpacktest1" "01010101" "3099"] 
+(def args-list {["Test" "Wolfpacktest1" "01010101" "3099"] 
  ["Test" "Wolfpacktest3" "90340909" "3101"] 
  ["Test" "Wolfpacktest2" "01010101" "3100"] 
  ["Test" "Wolfpacktest5" "0101010010101" "3103"] 
@@ -44,7 +43,7 @@
  ["Test" "Wolfpacktest6" "01010101010010101" "3104"] 
  ["Test" "Wolfpacktest7" "1010101010" "3105"] 
  ["Thorax" "TestgebruikerZvJ2" "033 465 91 15" "3095"] 
- ["Thorax" "TestgebruikerZvJBeheer" "033-46591152" "3094"]}#)
+ ["Thorax" "TestgebruikerZvJBeheer" "033-46591152" "3094"]})
 
 ;(defn create-task [task-name]
  ; (sql/insert! item/spec
